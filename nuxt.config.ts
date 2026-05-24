0e17c10d9987282d0e42e4ba0f381116ef7517bf
andre <andrey.kozlovskiy.v@gmail.com>
2026-05-24 12:38:10 +0200

    fix 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     cssPath: '~/assets/css/main.css',
   },
   image: {
+    provider: 'ipx',
     format: ['avif', 'webp'],
     quality: 80,
     densities: [1, 2],
